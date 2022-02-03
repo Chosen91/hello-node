c70285a7f547981a48512050a208b005984b0646
Markteisha Gill <markteishagill@Markteishas-MacBook-Pro.local>
2022-02-03 15:41:47 -0500

    deleted white space

--- main.js
+++ main.js
@@ -13,4 +13,3 @@ console.log("HH   HH   eee   LLL  LLL  oooo ww     ww  oooo rr rr lll       dd")
 console.log("HHHHHHH ee   e  LLL  LLL  oo oo  ww  ww  oo  oo rrr r lll    dddd")
 console.log("HH  HH eeee lll lll  oo oo  ww ww ww oo oo rr     lll  dd    ddddd")
 console.log("HH  HH   eeeee lll  lll  oooo      ww  ww   oooo rr lll dd   ddddd")
-
