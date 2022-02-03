fe7d53b5a41c53eecfc24996ec14a7ce6be70f02
Markteisha Gill <markteishagill@Markteishas-MacBook-Pro.local>
2022-02-03 15:33:05 -0500

    finsihed

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("HELLO, WORLd")
+console.log("\n")
+console.log("Goodbye world!")
+
+let person = "guyy"
+let person2 = "Todd"
+
+let people = person + " " + person2 
+console.log(people)
+
+console.log("HH   HH         LLL  LLL                             lll       dd")
+console.log("HH   HH   eee   LLL  LLL  oooo ww     ww  oooo rr rr lll       dd")
+console.log("HHHHHHH ee   e  LLL  LLL  oo oo  ww  ww  oo  oo rrr r lll    dddd")
+console.log("HH  HH eeee lll lll  oo oo  ww ww ww oo oo rr     lll  dd    ddddd")
+console.log("HH  HH   eeeee lll  lll  oooo      ww  ww   oooo rr lll dd   ddddd")
+
